b7ecdaa02b307662ee4f4ccb84eb4f0417ef90d1
unknown <Raymond.NelsonII@my.scitexas.edu>
2018-11-24 18:17:50 -0500

    backend push up to date

--- models/bus.js
+++ models/bus.js
@@ -29,6 +29,7 @@ module.exports = function(sequelize, DataTypes) {
           
         }
       });
+      
     };
 
     return Bus;
--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,27 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/users', function(req, res, next) {
+router.get('/buses', function(req, res, next) {
+  models.bus.findAll({}).then(busAsPlainObject => {
+
+    res.send(busAsPlainObject);
+  });
+});
+
+router.get('/busDetails', function(req, res, next) {
   models.bus.findAll({include:['roster','busDriver']}).then(busAsPlainObject => {
 
     res.send(busAsPlainObject);
   });
 });
+
 router.get('/student', function(req, res, next) {
   models.student.findAll({}).then(studentAsPlainObject => {
 
     res.send(studentAsPlainObject);
   });
 });
+
 router.get('/driver', function(req, res, next) {
   models.driver.findAll({ where: { driver: req.query.name }, attributes: ['driver'] }).then(driver => {
     if (driver.length) {
@@ -31,6 +40,7 @@ router.get('/guardian', function(req, res, next) {
     res.send(guardianAsPlainObject);
   });
 });
+
 router.post('/guardian', (req, res) => {
   models.guardian
   .findOrCreate({
